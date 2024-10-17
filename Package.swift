698952cd82e37e88c81bff91fcb88171a3186347
thuannt2 <thuannt2@runsystem.net>
2024-10-17 20:47:33 +0700

    feat: #3 - whisper coreml

--- Package.swift
+++ Package.swift
@@ -44,7 +44,9 @@ let package = Package(
                 .unsafeFlags(["-Wno-shorten-64-to-32", "-O3", "-DNDEBUG"]),
                 .define("GGML_USE_ACCELERATE"),
                 .unsafeFlags(["-fno-objc-arc"]),
-                .define("GGML_USE_METAL")
+                // .define("GGML_USE_METAL")
+                .define("WHISPER_USE_COREML"),
+                .define("WHISPER_COREML_ALLOW_FALLBACK"),
                 // NOTE: NEW_LAPACK will required iOS version 16.4+
                 // We should consider add this in the future when we drop support for iOS 14
                 // (ref: ref: https://developer.apple.com/documentation/accelerate/1513264-cblas_sgemm?language=objc)
